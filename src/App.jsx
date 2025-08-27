57d649922af74ec168839fb51dc0cb9d15455a8e
Abdelouahab_a <wahabaourar00@gmail.com>
2025-08-27 15:36:49 +0100

    Finished NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Background } from "./components/Background"
 import { NavBar } from "./components/NavBar"
+import { Home } from "./pages/Home"
 function App() {
 
   return (
     <>
     <Background>
       <NavBar/>
+      <Home/>
     </Background>
     </>
   )
