d1a8e35450061b54456fb3b8e624df671cf73247
Abdelouahab_a <wahabaourar00@gmail.com>
2025-08-28 22:11:10 +0100

    started the about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,8 +4,19 @@
 
 export const About = () => {
     return(
-        <section id="about">
+        <section id="about" className="px-10">
             <h1 className="header"><span className="text-white">About</span> Me</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center border-4 border-primary rounded-xl p-10">
+                <div>
+                    <h1>Web Developer | Graphic Designer | AI Enthusiast</h1>
+                    <h1>Second Year Student At ENSIA</h1>
+                    <p>enjoys crafting user interfaces and building applications with clean and efficient codes</p>
+                    <p>Learner and explorer of fundamental technologies in computer science, with a growing interest in AI.</p>
+                </div>
+                <div>
+
+                </div>
+            </div>
         </section>
     )
 }
