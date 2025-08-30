6cbd927df1e1ffaf6aef9a23e330d319f97dab6a
Abdelouahab_a <wahabaourar00@gmail.com>
2025-08-30 10:55:30 +0100

    the skills icons

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,8 +1,8 @@
-
-
-
-
 export const Skills = () => {
+
+    const MySkills = [{
+
+    }]
     return(
         <section id="skills">
             <h1 className="header"><span className="text-white">My </span>Skills</h1>
