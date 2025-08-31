c04f2712cb12081e88982f5ce2255773c8e084b0
Abdelouahab_a <wahabaourar00@gmail.com>
2025-08-31 14:20:44 +0100

    finished projects with animations

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,5 @@
 import { ExternalLink, Github } from "lucide-react";
-
+import { motion } from "motion/react";
 
 export const Projects = () => {
 
@@ -36,18 +36,36 @@ export const Projects = () => {
     ];
     return(
         <section id="projects" className="px-10 max-md:px-4 overflow-hidden z-30">
-            <h1 className="header"><span className="text-white">Featured </span>Projects</h1>
+            <motion.h1 className="header"
+                initial={{opacity: 0}}
+                whileInView={{opacity: 1}}
+                transition={{duration: 0.5}}
+                viewport={{ once: false, amount: 0.4 }}
+            >
+                <span className="text-white">Featured </span>Projects
+            </motion.h1>
 
-            <p className="max-sm:text-sm text-glow">here are some of my recent projects. Each project was carefully crafted with attention to detail, performance, and user experience.</p>
+            <motion.p className="max-sm:text-sm text-glow"
+                initial={{opacity: 0}}
+                whileInView={{opacity: 1}}
+                transition={{duration: 0.5}}
+                viewport={{ once: false, amount: 0.4 }}
+            >here are some of my recent projects. Each project was carefully crafted with attention to detail, performance, and user experience
+            </motion.p>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-20 my-20">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-15 my-20 max-md:px-10 max-sm:px-2">
                 {projects.map((project, key) => {
                     return(
-                        <div key={key} className="rounded-xl card-hover bg-primary/5 z-30 backdrop-blur-lg shadow-2xl overflow-hidden relative">
+                        <motion.div key={key} className="rounded-xl card-hover bg-primary/5 z-30 backdrop-blur-lg shadow-2xl overflow-hidden relative"
+                            initial={{ opacity: 0}}
+                            whileInView={{ opacity: 1}}
+                            transition={{ duration: 0.3, ease: [0.5, 0.6, 0.8, 1] , delay: key * 0.5}}
+                            viewport={{ once: false, amount: 0.4 }} 
+                        >
                             <div className="h-48">
                                 <img src={project.image} alt={project.title} className="w-full h-full object-cover" />
                             </div>
-                            <div className="p-6">
+                            <div className="p-6 max-sm:p-2">
                                 <div className="flex flex-wrap gap-2 mb-4">
                                     {project.tags.map((tag) => {
                                         return(
@@ -57,9 +75,9 @@ export const Projects = () => {
                                         )
                                     })}
                                 </div>
-                                <h3 className="text-xl font-semibold mt-10 mb-1 text-primary text-glow">{project.title}</h3>
+                                <h3 className="text-xl font-semibold mt-10 mb-1 text-primary text-glow max-sm:text-lg">{project.title}</h3>
                                 <p className="text-sm mb-10">{project.description}</p>
-                            <div className="flex items-center justify-between fixed bottom-4 left-4">
+                            <div className="flex items-center justify-between absolute bottom-4 left-4">
                                 <div className="flex space-x-3">
                                     <a href={project.demoUrl} target="_blank" className="hover:text-primary transition-colors duration-500">
                                         <ExternalLink/>
@@ -71,7 +89,7 @@ export const Projects = () => {
                                 </div>
                             </div>
                             </div>
-                        </div>
+                        </motion.div>
                     )
                 })}
             </div>
