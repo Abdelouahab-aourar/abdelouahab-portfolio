2ca793bbfd410cabc2d25c9c5965a5e1e5c3824d
Abdelouahab_a <wahabaourar00@gmail.com>
2025-09-04 15:19:11 +0100

    the second part of contact section

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,34 @@
-import { Github, Instagram, Linkedin, Mail, MapPin, Phone } from "lucide-react"
+import { Github, Instagram, Linkedin, Mail, MapPin, Phone, Send } from "lucide-react"
 
 
 
 
 export const Contact = () => {
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "a554db19-f915-42d0-8bc4-e35721b5d4de");
+
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+
+        const res = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: json
+        }).then((res) => res.json());
+
+        if (res.success) {
+            console.log("Success", res);
+        }
+    };
+
     return(
-        <section id="contact" className="px-20 max-md:px-4 overflow-hidden z-30 mb-20">
+        <section id="contact" className="px-20 max-md:px-4 overflow-hidden z-30 pb-20">
             <h1 className="header"><span className="text-white">Get In </span>Touch</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
                 <div className="space-y-8">
@@ -63,10 +86,52 @@ export const Contact = () => {
                             </a>
 
                         </div>
-
                     </div>
+                </div>
+                <div className="bg-primary/5 p-8 rounded-2xl z-30 backdrop-blur-lg shadow-2xl border-1 border-primary card-hover">
+                    <h3 className="text-2xl font-semibold mb-6 ">Send a Message</h3>
 
+                    <form action="" className="space-y-6" onSubmit={onSubmit}>
+                        <div>
+                            <label htmlFor="name" className="block tex-sm font-medium mb-2">Your Name</label>
+                            <input 
+                            type="text" 
+                            id="name" 
+                            placeholder="Abdelouahab Aourar" 
+                            name="name" 
+                            required 
+                            className="w-full px-4 py-3 rounded-md border-2 border-primary bg-primary/10 focus:outline-hidden " 
+                            />
+                        </div>
 
+                        <div>
+                            <label htmlFor="email" className="block tex-sm font-medium mb-2">Your Email</label>
+                            <input 
+                            type="email" 
+                            id="email" 
+                            placeholder="abdelouahab.aourar@gmail.com" 
+                            name="email" 
+                            required 
+                            className="w-full px-4 py-3 rounded-md border-2 border-primary bg-primary/10 focus:outline-hidden " 
+                            />
+                        </div>
+
+                        <div>
+                            <label htmlFor="message" className="block tex-sm font-medium mb-2">Your Message</label>
+                            <textarea 
+                            id="message" 
+                            placeholder="Hello, I'd like to talk about ..." 
+                            name="message" 
+                            required 
+                            className="w-full px-4 py-3 rounded-md border-2 border-primary bg-primary/10 focus:outline-hidden resize-none" 
+                            />
+                        </div>
+
+                        <button type="submit" className="inline-flex items-center gap-2 cursor-pointer border-2 border-primary p-2 rounded-lg bg-primary hover:bg-background transition-all card-hover duration-500 ease-in-out active:bg-primary/10" >
+                            <p>Send Message</p>
+                            <Send size={14}/>
+                        </button>
+                    </form>
                 </div>
 
             </div>
