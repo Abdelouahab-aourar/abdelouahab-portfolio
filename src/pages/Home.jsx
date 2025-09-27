f660a067c3882196ac04b97ee6711ea9fd944ed3
Abdelouahab_a <wahabaourar00@gmail.com>
2025-09-27 10:55:12 +0100

    v 1.1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,25 @@
 import { Typewriter } from 'react-simple-typewriter'
-import HomeImg from '../assets/HomeImg.png'
 import { Github, Globe, Linkedin, Mail } from 'lucide-react'
-import { easeIn, easeInOut, easeOut, motion } from 'motion/react'
+import { motion } from 'motion/react'
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import { useState, useEffect } from 'react';
+
 
 export const Home = () => {
-    return(
+    const [width, setWidth] = useState(typeof window !== "undefined" ? window.innerWidth : 0);
+
+    useEffect(() => {
+        const handleResize = () => setWidth(window.innerWidth);
+        window.addEventListener("resize", handleResize);
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+    return (
         <section className='z-10 flex items-center px-10 py-20 w-full max-md:flex-col space-y-10 overflow-x-hidden' id='home'>
             <motion.div className='space-y-6 flex items-start flex-col w-[65%] text-left max-md:items-center max-md:text-center max-md:w-full'
-                initial={{opacity: 0, x: -50}}
-                whileInView={{opacity: 1, x: 0}}
-                transition={{duration: 0.5, ease: [0.5, 0.6, 0.8, 1]}}
+                initial={{ opacity: 0, x: -50 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.5, ease: [0.5, 0.6, 0.8, 1] }}
                 viewport={{ once: false, amount: 0.4 }}
             >
                 <h1 className='text-5xl font-bold max-[1100px]:text-4xl max-md:text-3xl'>Hello There!</h1>
@@ -35,13 +45,19 @@ export const Home = () => {
                     </button>
                 </a>
             </motion.div>
-            <motion.div className='w-[35%] flex items-center justify-center max-md:w-[75%]'
-                initial={{opacity: 0, x: 50}}
-                whileInView={{opacity: 1, x: 0}}
-                transition={{duration: 0.5, ease: [0.5, 0.6, 0.8, 1]}}
+            <motion.div className='w-[35%] flex items-center justify-center max-md:w-[80%] z-30'
+                initial={{ opacity: 0, x: 50 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.5, ease: [0.5, 0.6, 0.8, 1] }}
                 viewport={{ once: false, amount: 0.4 }}
             >
-                <img src={HomeImg} alt="Home Image" className='w-full h-full' />
+                <DotLottieReact
+                    src="/Coding.json"
+                    key={width}
+                    loop
+                    autoplay
+                    className='h-[450px] w-[450px] object-contain max-md:w-[300px] max-md:h-[300px]'
+                />
             </motion.div>
         </section>
     )
--- src/pages/SkillsCards.jsx
+++ src/pages/SkillsCards.jsx
@@ -39,6 +39,7 @@ export const SkillsCards = () => {
                 {categories.map((category, key) => {
                     return(
                         <motion.button
+                        key={key}
                         initial={{ opacity: 0, x: key > 2 ? 20 : key === 2 ? 0 : -20 }}
                         whileInView={{ opacity: 1, x: 0 }}
                         transition={{ duration: 0.1, ease: [0.5, 0.6, 0.8, 1] , delay: key % 2 * 0.2}}
